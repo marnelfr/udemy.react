89aef9acca3774fe09d6cf383e0f45bdc23183cb
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-07 01:14:27 +0100

    Until 'Adding normal js code'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
+    return (
+        <div>
+            <h1>We are starting !</h1>
+            {expenses.map(expense => {
+                return <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+            })}
+        </div>
+    );
 }
 
 export default App;
