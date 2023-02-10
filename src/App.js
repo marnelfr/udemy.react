1582c99f75910500a20b8ebf74f452b3a26ba7ff
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-10 11:33:07 +0100

    The simplest solution I though about regarding the presented login system

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Home from './components/section10/Home/Home';
 import MainHeader from './components/section10/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('IsLoggedIn') === '1');
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('IsLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('IsLoggedIn')
     setIsLoggedIn(false);
   };
 
