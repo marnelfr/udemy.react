c15866bc61b7cc7d4c7f940b0179ce150f66ec02
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-10 12:34:01 +0100

    Cleanning up code

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ import Home from './components/section10/Home/Home';
 import MainHeader from './components/section10/MainHeader/MainHeader';
 
 function App() {
-  console.log(1);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(2);
+
   useEffect(() => {
     const isAlreadyLoggedIn = localStorage.getItem('IsLoggedIn') === '1'
-    console.log(3);
     if(isAlreadyLoggedIn) {
         setIsLoggedIn(true)
     }
   }, [])
-  console.log(4);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     localStorage.setItem('IsLoggedIn', '1')
     setIsLoggedIn(true);
   };
-  console.log(5);
+
   const logoutHandler = () => {
     localStorage.removeItem('IsLoggedIn')
     setIsLoggedIn(false);
   };
-console.log(6);
+
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
