fadff683b891df05831ccde66baf80aff33a48a2
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-12 22:33:18 +0100

    Used context to render total item added to the cart

--- src/components/Section11/Layout/HeaderCartButton.js
+++ src/components/Section11/Layout/HeaderCartButton.js
@@ -2,17 +2,19 @@ import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 import {useContext} from "react";
 import ModalContext from "../../../store/Section11/modal-context";
+import CartContext from "../../../store/Section11/cart-context";
 
 const HeaderCartButton = props => {
-  const context = useContext(ModalContext)
+  const modalContext = useContext(ModalContext)
+  const cartContext = useContext(CartContext)
 
   return (
-    <button onClick={context.showModalHandler} className={styles.button}>
+    <button onClick={modalContext.showModalHandler} className={styles.button}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{cartContext.cart.items.length}</span>
     </button>
   );
 };
