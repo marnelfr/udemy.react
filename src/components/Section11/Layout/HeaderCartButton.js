fa3c6eb3305d1e50d9912b741f96105ecf7f2c7e
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-13 00:44:01 +0100

    Add animation to the hearderCartBtn

--- src/components/Section11/Layout/HeaderCartButton.js
+++ src/components/Section11/Layout/HeaderCartButton.js
@@ -1,20 +1,39 @@
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import ModalContext from "../../../store/Section11/modal-context";
 import CartContext from "../../../store/Section11/cart-context";
 
 const HeaderCartButton = props => {
   const modalContext = useContext(ModalContext)
   const cartContext = useContext(CartContext)
+  const [isAnimated, setIsAnimated] = useState(false)
+
+  const btnClasses = `${styles.button} ${isAnimated ? styles.bump : ''}`
+
+  const totalItemsInTheCart = cartContext.cart.items.length;
+  useEffect(() => {
+    if(totalItemsInTheCart === 0) {
+      return
+    }
+
+    setIsAnimated(true)
+    const timer = setTimeout(() => {
+      setIsAnimated(false)
+    }, 500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [totalItemsInTheCart])
 
   return (
-    <button onClick={modalContext.showModalHandler} className={styles.button}>
+    <button onClick={modalContext.showModalHandler} className={btnClasses}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>{cartContext.cart.items.length}</span>
+      <span className={styles.badge}>{totalItemsInTheCart}</span>
     </button>
   );
 };
