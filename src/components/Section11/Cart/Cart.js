5ca1fc49c2899f93ba5d7a7f00c16b55bb7ed8e1
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-13 12:34:32 +0100

    Working on the modal component

--- src/components/Section11/Cart/Cart.js
+++ src/components/Section11/Cart/Cart.js
@@ -1,8 +1,20 @@
 import styles from './Cart.module.css'
 
-const Cart = () => {
+const Cart = props => {
+  const cartItems = [{id: 'c1', name: 'Sushi', amount: 3, price: 12.33}].map(item => <li>{item.name}</li>)
+
   return (
-    <></>
+    <>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>35</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']}>Close</button>
+        <button className={styles.button}>Order</button>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Section11/UI/Modal/Modal.js
@@ -0,0 +1,21 @@
+import styles from './Modal.module.css'
+
+const Backdrop = props => <div className={styles.backdrop}/>
+
+const ModalContent = props => {
+  return (
+    <div>
+      <div>
+        {}
+      </div>
+    </div>
+  )
+}
+
+const Modal = props => {
+  return (
+    <></>
+  )
+}
+
+export default Modal;
