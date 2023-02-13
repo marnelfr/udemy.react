1d1ec8eec53e655d2f48f622ebf3a416c92bd632
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-13 14:01:08 +0100

    Used the cart context in the header

--- src/components/Section11/Layout/HeaderCartButton.js
+++ src/components/Section11/Layout/HeaderCartButton.js
@@ -2,9 +2,12 @@ import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 import {useContext} from "react";
 import ModalContext from "../../../store/Modal/modal-context";
+import CartContext from "../../../store/Cart/cart-context";
 
 const HeaderCartButton = props => {
   const { showModalHandler } = useContext(ModalContext)
+  const { items } = useContext(CartContext)
+  const totalItemAdded = items.length
 
   return (
     <button onClick={ showModalHandler } className={styles.button}>
@@ -12,7 +15,7 @@ const HeaderCartButton = props => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{ totalItemAdded }</span>
     </button>
   );
 };
