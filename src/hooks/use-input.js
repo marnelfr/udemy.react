34e65224fe9ff226e31b6c4e62657081cb10a60b
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-16 16:05:38 +0100

    Using useReducer in our useInput

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,24 +1,26 @@
-import {useState} from "react";
+import {useReducer, useState} from "react";
+
+const initialState = {value: '', isTouched: false}
+
+const inputReducer = (state, action) => {
+  if(action.type === 'CHANGE') {
+    return {...state, value: action.value}
+  }
+  if(action.type === 'BLUR') {
+    return {...state, isTouched: true}
+  }
+  return initialState
+}
 
 const useInput = (validator) => {
-  const [value, setValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [{value, isTouched}, dispatch] = useReducer(inputReducer, initialState)
 
   const valueIsValid = validator(value);
   const inputIsInvalid = !valueIsValid && isTouched;
 
-  const changeHandler = (event) => {
-    setValue(event.target.value);
-  };
-
-  const blurHandler = () => {
-    setIsTouched(true);
-  };
-
-  const reset = () => {
-    setValue('')
-    setIsTouched(false)
-  }
+  const reset = () => dispatch({type: 'RESET'})
+  const blurHandler = () => dispatch({type: 'BLUR'});
+  const changeHandler = (event) => dispatch({type: 'CHANGE', value: event.target.value});
 
   const classes = inputIsInvalid
     ? 'form-control invalid'
