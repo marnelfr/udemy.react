778f4a210fb1a5a40616dbe6632c225aa626a556
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-17 19:30:21 +0100

    Adding html components

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import CartIcon from '../Cart/CartIcon';
+import styles from './HeaderCartButton.module.css';
+
+const HeaderCartButton = props => {
+  return (
+    <button className={`${styles.button} ${styles.bump}`}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>45</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import styles from './MealItem.module.css'
+import MealItemForm from "./MealItemForm";
+
+const MealItem = props => {
+  return (
+    <li className={styles.meal}>
+      <div>
+        <h3>Meal's name</h3>
+        <div className={styles.description}>Meal's description</div>
+        <div className={styles.price}>15,25</div>
+      </div>
+      <div>
+        <MealItemForm/>
+      </div>
+    </li>
+  )
+}
+
+export default MealItem
