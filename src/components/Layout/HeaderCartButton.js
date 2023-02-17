efedc44a5b212c6fa68d5110470cdbfa17b32a71
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-18 00:17:20 +0100

    Working on the btn cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,30 @@
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import ModalContext from "../../store/modal-context";
 import CartContext from "../../store/CartContext/cart-context";
 
 const HeaderCartButton = props => {
   const { showModalHandler } = useContext(ModalContext)
   const { items } = useContext(CartContext)
+  const [isAnimated, setIsAnimated] = useState(true)
   const totalItem = items.reduce((prevValue, item) => prevValue + item.amount, 0)
   let btnClasses = `${styles.button} ${styles.bump}`
 
   useEffect(() => {
+    if(totalItem === 0) {
+      return
+    }
     const timer = setTimeout(() => {
-      btnClasses = styles.button
-    }, 300)
+      setIsAnimated(false)
+    }, 500)
+
     return () => clearTimeout(timer)
   }, [totalItem])
+
+  if(!isAnimated) {
+    btnClasses = styles.button
+  }
 
   return (
     <button onClick={showModalHandler} className={btnClasses}>
