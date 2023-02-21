24ae7b21310a9a43d2031e6a4496229b20505b67
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-21 18:27:58 +0100

    Added dummy products data

--- src/components/Section19/Shop/Products.js
+++ src/components/Section19/Shop/Products.js
@@ -1,17 +1,40 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    title: 'MacBook',
+    price: 2340,
+    description: 'This is a super powerful computer'
+  },
+  {
+    id: 'p2',
+    title: 'Display touch',
+    price: 1245,
+    description: 'The best screen for computer ever'
+  },
+  {
+    id: 'p3',
+    title: 'iPhone 13 Pro',
+    price: 1134.99,
+    description: 'The phone I offered to my (I hope) girlfriend'
+  }
+]
+
+
 const Products = (props) => {
+  const productList = DUMMY_PRODUCTS.map(product => <ProductItem
+    key={product.id}
+    title={product.title}
+    price={product.price}
+    description={product.description}
+  />)
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
+      <ul>{productList}</ul>
     </section>
   );
 };
