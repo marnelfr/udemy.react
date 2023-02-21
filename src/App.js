44c277bd680b66f70a9a8e5efb1ea2867d2fc4d6
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-21 11:13:37 +0100

    Added file to review the section 18 before starting the section 19

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import Counter from './components/Section18/Counter';
-import Auth from "./components/Section18/Auth";
-import Header from "./components/Section18/Header";
-import {useSelector} from "react-redux";
-import UserProfile from "./components/Section18/UserProfile";
+import Counter from './components/Section18.Revision/Counter';
 
 
 function App() {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
   return (
-    <>
-      <Header />
-      {isLoggedIn ? <UserProfile/> : <Auth/>}
-      <Counter />
-    </>
+    <Counter />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,42 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'
 
-// import ExpenseApp from './components/ExpenseApp/App';
-import reportWebVitals from './reportWebVitals';
-// import FormApp from "./components/Section16/App";
-// import GoalApp from "./components/CourseGoals/App";
-// import Section8App from "./components/Section8/App";
-// import LoginApp from "./components/section10/App"
-// import {AuthContextProvider} from "./store/Section10/auth-context";
-// import FoodApp from "./components/Section11Jsx/App";
-// import MovieApp from "./components/Section14/App";
-// import CounterApp from "./components/Section15/App";
-// import HttpApp from "./components/Section15.2/App";
-import App from "./App";
-import store from './store/Section18/index'
 import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/*todo: add a first page with two btn that let choose the app we wanna use between those two*/}
-    {/*<ExpenseApp/>*/}
-    {/*<GoalApp/>*/}
-    {/*<Section8App />*/}
-    {/*<AuthContextProvider><LoginApp /></AuthContextProvider>*/}
-    {/*<FoodApp />*/}
-    {/*<MovieApp />*/}
-    {/*<CounterApp/>*/}
-    {/*<HttpApp />*/}
-    {/*<FormApp/>*/}
-    <Provider store={store}>
-      <App/>
-    </Provider>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
