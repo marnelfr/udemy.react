f7f80c068a76d27c717213a1ef32dbb90a957bc2
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-27 15:48:12 +0100

    Working on the MainNavigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Layouts/Root";
 
 // Challenge / Exercise
 
@@ -22,8 +29,23 @@ import './App.css'
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {index: true, element: <HomePage />},
+      {path: 'events', element: <EventsPage />},
+      {path: 'events/:eventId', element: <EventDetailPage />},
+      {path: 'events/new', element: <NewEventPage />},
+      {path: 'events/:eventId/edit', element: <EditEventPage />}
+    ]
+  }
+])
+
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Section15/Card.js
+++
@@ -1,7 +0,0 @@
-import classes from './Card.module.css';
-
-const Card = (props) => {
-  return <div onClick={props.onClick} className={classes.card}>{props.children}</div>;
-};
-
-export default Card;
