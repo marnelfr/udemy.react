0846e325723fad51304bc8d8aec1d499bf2bd6de
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-27 01:14:10 +0100

    Added automatic logout

--- src/pages/Section20.2/Layout/Root.js
+++ src/pages/Section20.2/Layout/Root.js
@@ -1,7 +1,21 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useLoaderData, useSubmit} from "react-router-dom";
 import MainNavigation from "../../../components/Section20.2/MainNavigation";
+import {useEffect} from "react";
 
 const RootLayout = () => {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if(!token) {
+      return;
+    }
+    const timerId = setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'});
+    }, 1* 60 * 60 * 1000);
+    return () => clearTimeout(timerId)
+  }, [token, submit])
+
   return (
     <>
       <MainNavigation />
