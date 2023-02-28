83fcf089c06ce1f75319e52d9543984aef55f5b4
Marnel Gnacadja <gmlginolias@gmail.com>
2023-02-28 15:07:48 +0100

    Handled show/hide the modal with animation

--- src/components/Section24/Backdrop/Backdrop.js
+++ src/components/Section24/Backdrop/Backdrop.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Backdrop.css';
 
 const backdrop = (props) => (
-    <div className={`Backdrop ${props.show ? 'open' : 'close'}`}></div>
+    <div onClick={props.closed} className={`Backdrop ${props.show ? 'open' : 'close'}`}></div>
 );
 
 export default backdrop;
