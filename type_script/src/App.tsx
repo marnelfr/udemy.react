09dc63505b2f412c451f3105eaa28d05f745f0ae
Marnel Gnacadja <gmlginolias@gmail.com>
2023-03-07 20:43:58 +0100

    Got ready for practicing

--- type_script/src/App.tsx
+++ type_script/src/App.tsx
@@ -1,29 +1,17 @@
-import React, {useState} from 'react';
-import Todos from "./components/Todo/Todos";
-import Todo from "./modeles/todo";
-import NewTodo from "./components/NewTodo/NewTodo";
-import {TodoContextProvider} from "./store/todo-context";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-    const [todoList, setTodoList] = useState<Todo[]>([])
-
-    const removeTodoHandler = (id: number) => {
-      setTodoList((list) => {
-        return list.filter(todo => todo.id !== id)
-      })
-    }
-
-    const newTodoHandler = (text: string) => {
-        const todo = new Todo(text)
-        setTodoList(list => list.concat(todo))
-    }
-
-    return (
-        <TodoContextProvider>
-            <NewTodo />
-            <Todos />
-        </TodoContextProvider>
-    )
+  return (
+    <>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ type_script/src/components/Cart/Cart.tsx
@@ -0,0 +1,29 @@
+import styles from './Cart.module.css'
+
+import CartItem from "./CartItem";
+import Checkout from "./Checkout/Checkout";
+
+const Cart = props => {
+  return (
+    <div>
+      <CartItem/>
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>31,10</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']}>Close</button>
+        <button className={styles.button}>Order</button>
+      </div>
+      <Checkout />
+
+      <p>Your order has been submitted successfully</p>
+      <small>Please, check your email</small>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']}>Close</button>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
