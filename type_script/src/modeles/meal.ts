453711c0d8f8052937d93452f8486e7a563b25d9
Marnel Gnacadja <gmlginolias@gmail.com>
2023-03-10 05:53:40 +0100

    Adding the cart slice

---
+++ type_script/src/modeles/meal.ts
@@ -0,0 +1,11 @@
+export default class Meal {
+  name: string;
+  description: string;
+  price: number;
+
+  constructor(name: string, description: string, price: number) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+}
---
+++ type_script/src/redux/cart.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Meal from "../modeles/meal";
+
+type StateItemType = {
+  meal: Meal;
+  amount: number;
+};
+
+type InitialStateType = {
+  items: StateItemType[];
+  total: number;
+};
+
+const initialState: InitialStateType = {
+  items: [],
+  total: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    add(state, action) {},
+    remove(state, action) {},
+    reset(state) {},
+  },
+});
