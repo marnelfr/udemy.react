64da520eab9f380e9d083b2d98b1ab2d567be9de
Marnel Gnacadja <gmlginolias@gmail.com>
2023-03-22 23:25:56 +0100

    Added some security to the admin page

--- type_script/src/pages/Layout/Admin.tsx
+++ type_script/src/pages/Layout/Admin.tsx
@@ -1,6 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useAppSelector } from "../../redux/hooks";
 
-const AdminLayout = () => {
+const AdminLayout: React.FC = () => {
+  const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
+
   return (
     <>
       Admin layout
