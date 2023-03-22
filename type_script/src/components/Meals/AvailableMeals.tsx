f70071fafb92a31f0dd8282beba4266c9eed6673
Marnel Gnacadja <gmlginolias@gmail.com>
2023-03-22 12:10:04 +0100

    Clean up

--- type_script/src/components/Meals/AvailableMeals.tsx
+++ type_script/src/components/Meals/AvailableMeals.tsx
@@ -3,12 +3,10 @@ import MealItem from "./MealItem/MealItem";
 
 import styles from "./AvailableMeals.module.css";
 import { useAppSelector } from "../../redux/hooks";
-import config from "../../utils/config";
 
 const AvailableMeals: React.FC = () => {
   const meals = useAppSelector((state) => state.stock.meals);
   const mealList = meals.map((meal) => <MealItem meal={meal} key={meal.id} />);
-  console.log(config.backendURL);
   return (
     <section className={styles.meals}>
       <Card>{mealList}</Card>
